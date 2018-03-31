465aee7ad84a014795297817f2fa21760df7d0b6
Stefan Arentz <stefan@arentz.ca>
2018-03-31 13:43:19 -0400

    Updates

--- README.md
+++ README.md
@@ -25,7 +25,27 @@ To get this going in Firefox, you need the following:
 * Firefox Nightly (Or possibly Firefox 60 Beta or later, not sure)
 * Set `network.trr.url` to your `https://my.home.server.com/dns-query`
 * Set `network.trr.mode` to something higher than 1 (See [TRR Preferences](https://gist.github.com/bagder/5e29101079e9ac78920ba2fc718aceec))
-* Set `network.trr.useGET` to `true`
 
-I had to restart Firefox before it picked up these settings.
+I had to restart Firefox before it picked up these settings. You should see something like this appear:
 
+```
+2018/03/31 13:41:25 POST Request for <cdn-traffic-director.krxd.net./IN/AAAA>
+2018/03/31 13:41:25 POST Request for <cdn-fastly.krxd.net./IN/AAAA>
+2018/03/31 13:41:25 POST Request for <cdn-fastly.krxd.net.c.global-ssl.fastly.net./IN/AAAA>
+2018/03/31 13:41:25 POST Request for <cdn-traffic-director.krxd.net./IN/A>
+2018/03/31 13:41:25 POST Request for <cdn-fastly.krxd.net./IN/A>
+2018/03/31 13:41:25 POST Request for <cdn-fastly.krxd.net.c.global-ssl.fastly.net./IN/A>
+2018/03/31 13:41:36 POST Request for <www.telegraaf.nl./IN/A>
+2018/03/31 13:41:36 POST Request for <www.telegraaf.nl./IN/AAAA>
+2018/03/31 13:41:37 POST Request for <tmgonlinemedia.nl./IN/A>
+2018/03/31 13:41:37 POST Request for <tmgonlinemedia.nl./IN/AAAA>
+2018/03/31 13:41:37 POST Request for <cd.tnet.nl./IN/A>
+2018/03/31 13:41:37 POST Request for <cd.tnet.nl./IN/AAAA>
+2018/03/31 13:41:41 POST Request for <ocsp.pki.goog./IN/AAAA>
+2018/03/31 13:41:41 POST Request for <ocsp.pki.goog./IN/A>
+2018/03/31 13:41:41 POST Request for <www3.l.google.com./IN/A>
+2018/03/31 13:41:41 POST Request for <www3.l.google.com./IN/AAAA>
+2018/03/31 13:41:47 POST Request for <accounts.tnet.nl./IN/A>
+```
+
+Enjoy.
--- main.go
+++ main.go
@@ -6,9 +6,12 @@ package main
 
 import (
 	"encoding/base64"
-	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net"
 	"net/http"
+	"github.com/miekg/dns"
 )
 
 func lookup(query []byte) ([]byte, error) {
@@ -37,20 +40,62 @@ func lookup(query []byte) ([]byte, error) {
 	return buffer[:n], nil
 }
 
+func parseHostFromQuery(query []byte) string {
+	msg := &dns.Msg{}
+	if err := msg.Unpack(query); err != nil {
+		return "<unpack-error>"
+	}
+	s := ""
+	for i := 0; i < len(msg.Question); i++ {
+		if len(s) != 0 {
+			s += ", "
+		}
+		n := msg.Question[i].Name
+		c := dns.Class(msg.Question[i].Qclass).String()
+		t := dns.Type(msg.Question[i].Qtype).String()
+		s += fmt.Sprintf("%s/%s/%s", n, c, t)
+	}
+	return s
+}
+
 func queryHandler(w http.ResponseWriter, r *http.Request) {
-	dns := r.URL.Query().Get("dns")
-	if dns == "" {
+	var query []byte
+
+	switch r.Method {
+	case "GET":		
+		encoded := r.URL.Query().Get("dns")
+		if encoded == "" {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+		
+		dns, err := base64.RawURLEncoding.DecodeString(encoded)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+
+		query = dns
+	case "POST":
+		if r.Header.Get("Content-Type") != "application/dns-udpwireformat" {
+			http.Error(w, http.StatusText(http.StatusUnsupportedMediaType), http.StatusUnsupportedMediaType)
+			return			
+		}
+		
+		defer r.Body.Close()
+
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return			
+		}
+		query = body
+	default:
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
 
-	query, err := base64.RawURLEncoding.DecodeString(dns)
-	if err != nil {
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		return
-	}
-
-	println(hex.Dump(query))
+	log.Printf("%s Request for <%s>\n", r.Method, parseHostFromQuery(query))
 
 	response, err := lookup(query)
 	if err != nil {
